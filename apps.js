fa63b675946e6c52e7b5a7efb4631888ec2924c6
Ein_Green <mbananola2002@gmail.com>
2021-03-16 20:52:01 -0700

    added if, else if, and else rules to js

--- apps.js
+++ apps.js
@@ -1,16 +1,27 @@
-var userAge = "14";
-var _subscription = "1";
+var userAge = "18";
+var is_subscription = "1";
 
 if (userAge >= 18) {
-    console.log("Welcome!");
+    console.log("User is an Adult");
 } else if (userAge <= 10) {
-    alert("you are not allowed on this site")
+    alert("you are not allowed on this site");
 } else {
-    console.log("bruh, you got parental permission?")
+    console.log("User is a teenager");
 }
 
-if (_subscription == 1) {
-    console.log("user is subscribed")
+if (is_subscription == true) {
+    console.log("user is subscribed");
 } else {
-    console.log("user is not subscribed")
+    console.log("user is not subscribed");
 }
+
+if (userAge>= 18 && is_subscription == true) {
+    console.log("user is an Adult and Subscribed")
+} else if (userAge>= 18 && is_subscription == false) {
+    console.log("user is an Adult but not Subscribed")
+} else if (userAge<= 18 && is_subscription == ture) {
+    console.log("user is not an Adult but is Subscribed")
+} else {
+    console.log("user is not an Adult and not Subscribed")
+}
+
