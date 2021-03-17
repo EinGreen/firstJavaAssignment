b5abf4947076cf06d1e91c6d45a1cff8c15c6522
Ein_Green <mbananola2002@gmail.com>
2021-03-16 20:29:50 -0700

    started first Java Script assignment!

---
+++ apps.js
@@ -0,0 +1,16 @@
+var userAge = "14";
+var _subscription = "1";
+
+if (userAge >= 18) {
+    console.log("Welcome!");
+} else if (userAge <= 10) {
+    alert("you are not allowed on this site")
+} else {
+    console.log("bruh, you got parental permission?")
+}
+
+if (_subscription == 1) {
+    console.log("user is subscribed")
+} else {
+    console.log("user is not subscribed")
+}
